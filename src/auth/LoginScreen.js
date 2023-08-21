9e440695b6592998fdedec0d4660899467b5f2e0
dunghmph18450 <you@example.com>
2023-08-21 22:48:44 +0700

    fist up

---
+++ src/auth/LoginScreen.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+
+export default function LoginScreen() {
+  return (
+    <SafeAreaView>
+        <TextInput></TextInput>
+      <Text>LoginScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
