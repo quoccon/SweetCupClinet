f180029c39e9e8d0d7c8f5103d36e7f1833260dc
dunghmph18450 <you@example.com>
2023-08-26 01:07:27 +0700

    test login

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,10 +1,44 @@
 import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import api from '../../api/axios'
+import axios from 'axios'
 
 export default function LoginScreen() {
+  const [userName,setuserName] = useState(" ")
+  const [passwd,setpasswd] = useState(" ")
+  
+
+ 
+
+  const checkLoginU =  async () =>{
+    try {
+      console.log("Đến đây oke")
+      const res =  await api.post("/login",{
+        username : userName,
+        passwd: passwd
+      })
+      
+      if (res.status === 200) {
+        console.log("Đăng nhập thành công")
+      }else(console.log(res.status))
+    } catch (error) {
+      console.log(error)
+      console.log("lõi")
+    }
+
+     
+  }
+
   return (
-    <SafeAreaView>
-        <TextInput></TextInput>
+    <SafeAreaView style={{marginTop: 15}}>
+        <TextInput placeholder='tài khoản' onChangeText={(username)=>{setuserName(username)}}></TextInput>
+        <TextInput placeholder='pass' onChangeText={(passwdd)=>{setpasswd(passwdd)}}></TextInput>
+        <TouchableOpacity onPress={() => {checkLoginU()}}>
+          <Text>
+            Đăng Nhập
+          </Text>
+        </TouchableOpacity>
       <Text>LoginScreen</Text>
     </SafeAreaView>
   )
