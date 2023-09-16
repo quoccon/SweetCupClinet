c022705f4be6212cc2c80f78650f72871ce37524
dunghmph18450 <you@example.com>
2023-09-16 21:48:46 +0700

    fix redux cart

--- src/component/Cart/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStire, configureStore} from "@reduxjs/toolkit";
-import CartReducer from "./CartReducer";
-
-export default configureStore({
-    reducer: {
-        cart:CartReducer
-    }
-})
